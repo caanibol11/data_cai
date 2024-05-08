7d1695abfe82d42419bd0ab6f63023405fcf29b9
caanibol11 <carlos.andres.ibarra@gmail.com>
2024-05-08 00:09:01 -0500

    second-push

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates") ## carpeta templates
 
-@app.get("/",tags=["Página Principal"])
+@app.get("/",tags=["FUNCIONES PROYECTO MLOPS"])
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get('/funcion_uno', tags=['generos'])
 async def PlayTimeGenre(genero: str):
-    # Cargar los datos del archivo CSV (reemplaza 'data.csv' con tu archivo)
+    # Cargar los datos del archivo CSV 
     df = pd.read_csv('./datasets/funcion_uno.csv')
 
     # Filtrar el DataFrame por género
@@ -28,4 +28,49 @@ async def PlayTimeGenre(genero: str):
 
     # Devolver el resultado en un diccionario
     result = {"Año de lanzamiento con más horas jugadas para Género " + genero: max_year}
+    return result
+
+
+@app.get('/funcion_dos', tags=['generos'])
+async def UserForGenre(genero: str):
+    # Cargar los datos del archivo CSV 
+    df = pd.read_csv('./datasets/funcion_dos.csv')
+
+    # Filtrar el DataFrame por género
+    df_genre = df[df['genres'] == genero]
+
+    # Agrupar por usuario y año de lanzamiento, y calcular la suma de las horas jugadas para cada usuario y año
+    horas_usuario_anio = df_genre.groupby(['user_id', 'year'])['playtime_forever'].sum()
+
+    # Encontrar el usuario con la máxima suma de horas jugadas para el género dado
+    max_usuario = horas_usuario_anio.groupby(level='user_id').sum().idxmax()
+
+    # Filtrar las horas jugadas del usuario con la máxima suma
+    horas_usuario_max = horas_usuario_anio.loc[max_usuario].reset_index()
+
+    # Convertir los datos a un formato de lista de diccionarios
+    horas_por_anio = horas_usuario_max.to_dict(orient='records')
+
+    # Devolver el resultado en un diccionario
+    result = {
+       "Usuario con más horas jugadas para Género " + genero: max_usuario,
+        "Horas jugadas": horas_por_anio
+    }
+
+    return result
+
+@app.get('/funcion_tres', tags=['sentimiento'])
+async def sentiment_analysis(año: int):
+    # Cargar los datos del archivo CSV 
+    df= pd.read_csv('./datasets/funcion_tres.csv')
+
+    # Filtrar el DataFrame por el año de lanzamiento
+    df_year = df[df['year'] == año]
+
+    # Contar la cantidad de registros de reseñas categorizados con análisis de sentimiento
+    sentiment_counts = df_year['sentiment_analysis'].value_counts()
+
+    # Crear un diccionario con los recuentos de sentimientos
+    result = sentiment_counts.to_dict()
+
     return result
